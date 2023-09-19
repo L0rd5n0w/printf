c5d491f21830bdff3cd0540a94afc843fbb7d18d
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2023-09-19 18:32:21 +0100

    0987

---
+++ precision.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * get_precision -function
+ * @format: parameter
+ * @i: parameter
+ * @list: parameter
+ * Return:(0)Always
+ */
+int get_precision(const char *format, int *i, va_list list)
+{
+	int curry = *i + 1;
+	int precision = -1;
+
+	if (format[curry] != '.')
+		return (precision);
+
+	precision = 0;
+
+	for (curry += 1; format[curry] != '\0'; curry++)
+	{
+		if (is_digit(format[curry]))
+		{
+			precision *= 10;
+			precision += format[curry] - '0';
+		}
+		else if (format[curry] == '*')
+		{
+			curry++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = curry - 1;
+
+	return (precision);
+}
+
