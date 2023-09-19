dbb6de17cf27faf23be19728a81d179c2d325c45
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2023-09-19 18:38:03 +0100

    almost there

---
+++ get_width.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * get_width -funtion
+ * @format: parameter
+ * @i: parameter
+ * @list: parameeter
+ * Return:(0)Always
+ */
+int get_width(const char *format, int *i, va_list list)
+{
+	int curry;
+	int width = 0;
+
+	for (curry = *i + 1; format[curry] != '\0'; curry++)
+	{
+		if (is_digit(format[curry]))
+		{
+			width *= 10;
+			width += format[curry] - '0';
+		}
+		else if (format[curry] == '*')
+		{
+			curry++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = curry - 1;
+
+	return (width);
+}
+
---
+++ size.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * get_size -function
+ * @format: parameter
+ * @i: parameter
+ * Return:(0)Always 
+ */
+int get_size(const char *format, int *i)
+{
+	int curry = *i + 1;
+	int size = 0;
+
+	if (format[curry] == 'l')
+		size = S_LONG;
+	else if (format[curry] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*i = curry - 1;
+	else
+		*i = curry;
+
+	return (size);
+}
